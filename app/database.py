5ac06bda15569cfd88806c6d6c8611bbf2deb855
Ayuhoney <ayush2211207@gmail.com>
2025-08-05 09:21:15 +0530

    fix issue

--- app/database.py
+++ app/database.py
@@ -1,6 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URI = "mongodb://ayush2211207:koLO50CvMTHHJ1Wg@cluster0.d7fkhvy.mongodb.net:27017/immigration?ssl=false&authSource=admin"
-
+MONGO_URI = "mongodb+srv://ayush2211207:koLO50CvMTHHJ1Wg@cluster0.d7fkhvy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = AsyncIOMotorClient(MONGO_URI)
 db = client.immigration
